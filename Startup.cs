297596f45707ee3bc119ab95b2d51dade41dcbc9
Natalie Akam <natalie.akam@gmail.com>
2020-01-02 18:28:44 +0000

    Add heroku production config to startup

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore.Design;
+// using Microsoft.EntityFrameworkCore.Tools;
 
 using FoodBlog.Services;
 
@@ -18,42 +19,72 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FoodBlog
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-            var connectionString = Configuration.GetConnectionString("BlogContext");
-            services.AddEntityFrameworkNpgsql().AddDbContext<BlogContext>(options => options.UseNpgsql(connectionString));
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+      
+      services.AddEntityFrameworkNpgsql().AddDbContext<BlogContext>(options =>
+      {
+        var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        string connStr;
+        // Depending on if in development or production, use either Heroku-provided
+        // connection string, or development connection string from env var.
+        if (env == "Development")
+        {
+          // Use connection string from file.
+          connStr = Configuration.GetConnectionString("BlogContext");
+        }
+        else
+        {
+          // Use connection string provided at runtime by Heroku.
+          var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+          // Parse connection URL to connection string for Npgsql
+          connUrl = connUrl.Replace("postgres://", string.Empty);
+          var pgUserPass = connUrl.Split("@")[0];
+          var pgHostPortDb = connUrl.Split("@")[1];
+          var pgHostPort = pgHostPortDb.Split("/")[0];
+          var pgDb = pgHostPortDb.Split("/")[1];
+          var pgUser = pgUserPass.Split(":")[0];
+          var pgPass = pgUserPass.Split(":")[1];
+          var pgHost = pgHostPort.Split(":")[0];
+          var pgPort = pgHostPort.Split(":")[1];
+          connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+        }
+        // Whether the connection string came from the local development configuration file
+        // or from the environment variable from Heroku, use it to set up your DbContext.
+        options.UseNpgsql(connStr);
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
